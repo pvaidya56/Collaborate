fa28b81c5a59c59cac041cb22f57bc1dc84f053e
Priya <pvaidya56@gmail.com>
2020-09-12 15:53:18 -0700

    start top nav component

--- client/collaborate/src/components/dashboard/Layout.js
+++ client/collaborate/src/components/dashboard/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+// Components
+import TopNav from './TopNav';
+
 export default function Layout() {
     return (
         <div>
+            <TopNav />
             this is dashboard
             <Link to='/dashboard' />
         </div>
