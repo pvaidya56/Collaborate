36754843586d1f7de7d66c93ea945235cd0803a4
Priya <pvaidya56@gmail.com>
2020-09-11 20:08:54 -0700

    add react router

--- client/collaborate/src/App.js
+++ client/collaborate/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// Components 
+import Register from './components/register';
+import Login from './components/login';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Collaborate</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Collaborate</h1>
+        <Switch>
+          <Route exact path='/' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
