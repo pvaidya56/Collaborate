5224d29a8451643d851640fab00a1c5f309e33fa
Priya <pvaidya56@gmail.com>
2020-09-13 14:16:54 -0700

    create top nav

--- client/collaborate/src/components/dashboard/TopNav.js
+++ client/collaborate/src/components/dashboard/TopNav.js
@@ -9,7 +9,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton'
 import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons';
+// import MenuIcon from '@material-ui/core/MenuIcon';
 
 import NotificationsIcon from '@material-ui/icons/Notifications';
 
@@ -63,7 +63,7 @@ export default function TopNav() {
             // onClick={handleDrawerOpen}
             className={clsx(classes.menuButton && classes.menuButtonHidden)}
           >
-            <MenuIcon />
+            {/* <MenuIcon /> */}
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
             Dashboard
